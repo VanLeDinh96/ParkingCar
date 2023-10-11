06be2ad2baf76dea2132d2499a482596d295180b
VanLeDinh96 <dinhvanle.it@gmail.com>
2023-10-11 17:32:00 +0700

    update

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using MediatR;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddCQRS(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
